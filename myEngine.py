2e0b6d3874bc1cacdeceb63a1dcba9e715a7f07a
arnavCode <arnavcnyaupane@gmail.com>
2023-03-06 20:47:20 +0000

    Player is now able to make moves, legality not yet implemented

--- myEngine.py
+++ myEngine.py
@@ -16,7 +16,21 @@ class gameState():
             ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]
         ]
 
+    def makeMove(self, move):
+        self.board[move.startRow][move.startCol] = "--"
+        self.board[move.endRow][move.endCol] = move.pieceMoved
+        self.moveLog.append(move)
+
+#quite inexpensive to make moves
 class Move():
+    #maps keys to values
+    ranksToRows = {"1": 7, "2": 6, "3": 5, "4": 4,
+                    "5": 3, "6": 2, "7": 1, "8": 0}
+    rowsToRanks = {v: k for k, v in ranksToRows.items()} #reverse of ranksToRows
+    filesToCols = {"a": 0, "b": 1, "c": 2, "d": 3,
+                    "e": 4, "f": 5, "g": 6, "h": 7}
+    colsToFiles = {v: k for k, v in filesToCols.items()} #reverse of filesToCols
+
     def __init__(self, startSq, endSq, board):
         self.startRow = startSq[0]
         self.startCol = startSq[1]
@@ -24,4 +38,12 @@ class Move():
         self.endCol = endSq[1]
         self.pieceMoved = board[self.startRow][self.startCol]
         self.pieceCaptured = board[self.endRow][self.endCol]
-        self.moveID = self.startRow * 1000 + self.startCol * 100 + self.endRow * 10 + self.endCol
+
+
+
+    #not accurate to chess notation, only rank-file notation: does not include promotion, check, checkmate, etc.
+    def getChessNotation(self):
+        return self.getRankFile(self.startRow, self.startCol) + self.getRankFile(self.endRow, self.endCol)
+
+    def getRankFile(self, r, c):
+        return self.colsToFiles[c] + self.rowsToRanks[r]
